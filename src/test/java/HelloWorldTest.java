907a4d55674128789fba1777acc4bdd2298a4783
Vladislav Kichutov <v.kichutov@gmail.com>
2023-04-27 22:01:21 +0300

    Added GET test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,3 +1,5 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
@@ -5,5 +7,13 @@ public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
         System.out.println("Hello from Vladislav");
+    }
+
+    @Test
+    public void testGet() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
